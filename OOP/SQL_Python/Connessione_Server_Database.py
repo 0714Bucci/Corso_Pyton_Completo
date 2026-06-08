7256261d0a809fdbfdcfadc9792e5e5c33987f18
1407Bucci <lauraricordi@hotmaail.it>
2026-06-08 13:00:39 +0200

    update OOP

---
+++ OOP/SQL_Python/Connessione_Server_Database.py
@@ -0,0 +1,15 @@
+"""obiettivo del corso:
+collegamento a DataBAse SQL Server
+eseguire query del sistema
+recuperare automaticamente le tabelle
+getire errori e connessioni in modo pro
+capire come un Data Analyst esplora un DataBAse sconosciuto
+"""
+#pip install pyodbc
+import pyodbc  as adbc
+import pandas as pd
+
+#Stringa  di connessione 
+
+
+#Gestire gli errori 
